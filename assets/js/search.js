b1c6d81ecd906867af2424654e291bae7dd58deb
jacobbubu <rong.shen@gmail.com>
2018-06-09 02:51:37 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable","128":"Class"},"rows":[{"id":0,"kind":128,"name":"DummyClass","url":"classes/dummyclass.html","classes":"tsd-kind-class"},{"id":1,"kind":32,"name":"name","url":"globals.html#name","classes":"tsd-kind-variable"}]};
